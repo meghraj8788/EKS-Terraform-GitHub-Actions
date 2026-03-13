9c5cd96ab6a104ea569e1a277271251c7f6d68ea
Meghraj Fand <meghraj8788@gmail.com>
2026-03-13 23:59:27 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "dev-aman-tf-bucket"
-    region         = "eu-west-1"
+    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     use_lockfile = true
     encrypt        = true
