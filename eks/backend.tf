ca3008a2969c92aa2d2327dc93482a0806519152
Meghraj Fand <meghraj8788@gmail.com>
2026-03-14 13:57:47 +0530

    Fix typo in S3 bucket name

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-buckett"
+    bucket         = "dev-aman-tf-buckettt"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     use_lockfile = true
